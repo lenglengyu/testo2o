42f2446ffa1ca700bd48645f81e29766cdcd9914
lenglengyu <1219492368@qq.com>
2019-06-22 15:26:51 +0800

    修改了文件；

--- o2o/src/main/java/com/imooc/o2o/develepe/adapter/Adapter.java
+++ o2o/src/main/java/com/imooc/o2o/develepe/adapter/Adapter.java
@@ -1,9 +1,11 @@
 package com.imooc.o2o.develepe.adapter;
 
 public class Adapter extends Target{
+	
 
-	private Adepee adepee=new Adepee();
 	public void Request() {
 		adepee.specialRequest();
+               adepee.specialRequest();
+
 	}
 }
